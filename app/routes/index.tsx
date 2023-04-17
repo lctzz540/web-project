ac3078a4f999e5d70f3dd07b442e85ae296757bf
Thang Le <lctzz540@outlook.com>
2023-04-17 23:35:45 +0700

    add more excercises

---
+++ app/routes/index.tsx
@@ -0,0 +1,67 @@
+import type { V2_MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+import { useOptionalUser } from "~/utils";
+
+export const meta: V2_MetaFunction = () => [{ title: "Remix Notes" }];
+
+export default function Index() {
+  const user = useOptionalUser();
+  return (
+    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
+      <div className="relative sm:pb-16 sm:pt-8">
+        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
+            <div className="absolute inset-0">
+              <img
+                className="h-full w-full object-cover filter"
+                src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fgradepowerlearning.com%2Fwp-content%2Fuploads%2F2018%2F09%2Fhow-to-use-self-study.jpeg&f=1&nofb=1&ipt=e48c76ee8fb264d20c6d61537b6a25c1394c4f84b87df9e95f7e993e6f640148&ipo=images"
+                alt="Sonic Youth On Stage"
+              />
+              <div className="absolute inset-0 bg-[color:rgba(254,204,27,0.5)] mix-blend-multiply" />
+            </div>
+            <div className="relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pb-20 lg:pt-32">
+              <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
+                <span className="block uppercase text-yellow-500 drop-shadow-md">
+                  study monitoring
+                </span>
+              </h1>
+              {!user && (
+                <p className="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
+                  Join us for more effective learning today
+                </p>
+              )}
+              <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
+                {user ? (
+                  <Link
+                    to="/notes"
+                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
+                  >
+                    Welcome {user.email}
+                  </Link>
+                ) : (
+                  <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
+                    <Link
+                      to="/join"
+                      className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
+                    >
+                      Sign up
+                    </Link>
+                    <Link
+                      to="/login"
+                      className="flex items-center justify-center rounded-md bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600"
+                    >
+                      Log In
+                    </Link>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="mx-auto max-w-7xl py-2 px-4 sm:px-6 lg:px-8"></div>
+      </div>
+    </main>
+  );
+}
--- data/exercisesData.js
+++ data/exercisesData.js
@@ -1,11 +1,27 @@
 const exercisesData = [
   {
-    id: 'warriorpose',
-    title: 'Warrior Pose',
+    id: 'warriorpose1',
+    title: 'Warrior Pose 1',
     description: `Warrior Pose, also known as Virabhadrasana, is a standing yoga posture that is great for building strength, flexibility, and balance in the legs, hips, and core. There are three variations of the pose, each with slightly different benefits:`,
     imgSrc: '/static/images/warriorpose.jpeg',
-    href: '/projects/exercise/warriorpose',
+    href: '/projects/exercise/warriorpose1',
     video: '/static/warriorsample.mp4',
+  },
+  {
+    id: 'treepose',
+    title: 'Tree Pose',
+    description: `Warrior Pose, also known as Virabhadrasana, is a standing yoga posture that is great for building strength, flexibility, and balance in the legs, hips, and core. There are three variations of the pose, each with slightly different benefits`,
+    imgSrc: '/static/images/treepose.jpeg',
+    href: '/projects/exercise/treepose',
+    video: '/static/treeposesample.mp4',
+  },
+  {
+    id: 'warriorpose2',
+    title: 'Warrior Pose 2',
+    description: `Warrior Pose, also known as Virabhadrasana, is a standing yoga posture that is great for building strength, flexibility, and balance in the legs, hips, and core. There are three variations of the pose, each with slightly different benefits:`,
+    imgSrc: '/static/images/warriorpose2.jpeg',
+    href: '/projects/exercise/warriorpose2',
+    video: '/static/warriorsample2.mp4',
   },
 ]
 export default exercisesData
